3f17fb86c3ccfa2f63bc8421e735a4465954bbbe
saiteki-kai <giuseppe.magazu@gmail.com>
2022-04-01 13:46:38 +0200

    test: review update

--- test/review_repository_test.dart
+++ test/review_repository_test.dart
@@ -47,4 +47,38 @@ void main() async {
     expect(reviews[0].nextDate?.millisecondsSinceEpoch ?? double.infinity,
         lessThan(DateTime.now().millisecondsSinceEpoch));
   });
+
+  group("update a review", () {
+    test("review already present in the database", () async {
+      List<Review> reviews = box.getAll();
+      Review r = reviews[0];
+
+      final id1 = await repo.updateReview(r);
+      expect(id1, equals(r.id));
+      expect(reviews.length, 3);
+
+      final updatedReview = r.copyWith(ef: 2.3);
+
+      final id2 = await repo.updateReview(updatedReview);
+      expect(id2, equals(r.id));
+
+      expect(r.ef, 2.5);
+      expect(updatedReview.ef, 2.3);
+
+      reviews = box.getAll();
+      expect(reviews.length, 3);
+
+      // refetch instance
+      reviews = box.getAll();
+      r = reviews[0];
+      expect(r.ef, 2.3);
+      expect(reviews.length, 3);
+    });
+
+    test("review not present in the database", () async {
+      final newReview = createReviewByDate(null);
+      final id3 = await repo.updateReview(newReview);
+      expect(id3, equals(4));
+    });
+  });
 }
