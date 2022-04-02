982e0e01ee680e01b47acef56856124fe87bdea6
MagazzuGaetano <gaetanomagazzu3@gmail.com>
2022-04-02 17:53:49 +0200

    feat: add word details route

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,11 +1,13 @@
 import 'package:auto_route/auto_route.dart';
 
+import '../ui/screens/word_details_screen/word_details_screen.dart';
 import '../ui/screens/home_screen.dart';
 
 @MaterialAutoRouter(
   replaceInRouteName: 'Page,Route',
   routes: <AutoRoute>[
     AutoRoute(path: '/', page: HomeScreen),
+    AutoRoute(path: '/words/:id', page: WordDetailsScreen),
   ],
 )
 class $AppRouter {}
