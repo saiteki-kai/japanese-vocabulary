844752b30d4b6da3c2a8d88ea2a71f367653e94e
MagazzuGaetano <gaetanomagazzu3@gmail.com>
2022-04-02 16:07:01 +0200

    feat: add get word

--- lib/data/repositories/word_repository.dart
+++ lib/data/repositories/word_repository.dart
@@ -1,6 +1,17 @@
 import '../../objectbox.g.dart';
 import '../app_database.dart';
+import '../models/word.dart';
 
 class WordRepository {
   Future<Store> get _store async => await AppDatabase.instance.store;
+
+  Future<Word?> getWord(int id) async {
+    if (id <= 0) {
+      return null;
+    }
+    else {
+      final box = (await _store).box<Word>();
+      return box.get(id);
+    }
+  }
 }
