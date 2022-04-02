ea1e054f170776fa398c78cbf9df1382889f618a
MagazzuGaetano <gaetanomagazzu3@gmail.com>
2022-04-02 19:39:39 +0200

    add title subtitle widget

---
+++ lib/ui/screens/word_details_screen/widgets/title_subtitle_widget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class TitleSubtitleWidget extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final TextStyle? titleTextStyle;
+  final TextStyle? subtitleTextStyle;
+  final TextAlign? titleTextAlign;
+  final TextAlign? subtitleTextAlign;
+  final EdgeInsetsGeometry padding;
+  final CrossAxisAlignment crossAxisAlignment;
+  final MainAxisAlignment mainAxisAlignment;
+  final MainAxisSize mainAxisSize;
+
+  const TitleSubtitleWidget({
+    Key? key,
+    required this.title,
+    required this.subtitle,
+    this.titleTextStyle,
+    this.subtitleTextStyle,
+    this.titleTextAlign,
+    this.subtitleTextAlign,
+    this.padding = EdgeInsets.zero,
+    this.mainAxisAlignment = MainAxisAlignment.center,
+    this.crossAxisAlignment = CrossAxisAlignment.center,
+    this.mainAxisSize = MainAxisSize.min,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: padding,
+      child: Column(
+        mainAxisSize: mainAxisSize,
+        crossAxisAlignment: crossAxisAlignment,
+        mainAxisAlignment: mainAxisAlignment,
+        children: [
+          Text(
+            title,
+            textAlign: titleTextAlign,
+            style: titleTextStyle,
+          ),
+          Text(
+            subtitle,
+            textAlign: subtitleTextAlign,
+            style: subtitleTextStyle,
+          )
+        ],
+      ),
+    );
+  }
+}
