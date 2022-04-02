321d5c0e6321c1e55f8557f3324e106142f0390a
saiteki-kai <giuseppe.magazu@gmail.com>
2022-04-02 18:02:17 +0200

    feat: add review scheduling algorithm (SM2)

--- lib/utils/sm2.dart
+++ lib/utils/sm2.dart
@@ -1,8 +1,54 @@
+// https://www.supermemo.com/en/archives1990-2015/english/ol/sm2
 import '../data/models/review.dart';
 
 class SM2 {
   static Review schedule(Review review, int quality) {
-    // TODO: implement
-    return review;
+    if (quality > 5 || quality < 0) {
+      throw const FormatException("The quality must be in the range [0, 5].");
+    }
+
+    double ef = review.ef;
+    int repetition = review.repetition;
+    int interval = review.interval;
+
+    int corrects = review.correctAnswers;
+    int incorrects = review.incorrectAnswers;
+
+    if (quality >= 3) {
+      if (repetition == 0) {
+        interval = 1;
+        repetition = 1;
+      } else if (repetition == 1) {
+        interval = 6;
+        repetition = 2;
+      } else {
+        interval = (interval * ef).round();
+        repetition = repetition + 1;
+      }
+
+      corrects = corrects + 1;
+    } else {
+      repetition = 0;
+      interval = 1;
+
+      incorrects = incorrects + 1;
+    }
+
+    ef = ef + (0.1 - (5 - quality) * (0.08 + (5 - quality) * 0.02));
+
+    if (ef < 1.3) {
+      ef = 1.3;
+    }
+
+    final next = DateTime.now().add(Duration(days: interval + 100));
+
+    return review.copyWith(
+      ef: ef,
+      interval: interval,
+      repetition: repetition,
+      correctAnswers: corrects,
+      incorrectAnswers: incorrects,
+      nextDate: next,
+    );
   }
 }
