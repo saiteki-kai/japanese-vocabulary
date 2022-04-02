076df3c6f6113feb319c4759cf15f7a6be9ebe2e
MagazzuGaetano <gaetanomagazzu3@gmail.com>
2022-04-02 19:40:15 +0200

    feat: add stats item widget

---
+++ lib/ui/screens/word_details_screen/widgets/stats_item_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class StatsItemWidget extends StatelessWidget {
+
+  final String label;
+  final String value;
+
+  const StatsItemWidget({
+    Key? key,
+    required this.label,
+    required this.value,
+  }) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          label,
+          style: const TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: Text(value),
+        ),
+      ],
+    );
+  }
+}
