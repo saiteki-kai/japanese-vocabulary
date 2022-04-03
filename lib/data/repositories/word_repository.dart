949bb29aadc2685785d2d30e497b13bad3e27c21
MagazzuGaetano <gaetanomagazzu3@gmail.com>
2022-04-03 17:28:47 +0200

    docs: add documentation for the get word method

--- lib/data/repositories/word_repository.dart
+++ lib/data/repositories/word_repository.dart
@@ -3,13 +3,16 @@ import '../app_database.dart';
 import '../models/word.dart';
 
 class WordRepository {
+  /// A database instance.
   Future<Store> get _store async => await AppDatabase.instance.store;
 
+  /// Returns the word with the specified [id].
+  ///
+  /// Returns null if the word not exists or the [id] is not valid.
   Future<Word?> getWord(int id) async {
     if (id <= 0) {
       return null;
-    }
-    else {
+    } else {
       final box = (await _store).box<Word>();
       return box.get(id);
     }
