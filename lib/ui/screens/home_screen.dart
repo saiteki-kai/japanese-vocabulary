9da97b9c72d181299e3c17a65d5616b02ece2cd3
Mirco Malanchini <mircomalanchini@hotmail.it>
2022-04-04 14:04:59 +0200

    feat: add integration for insert and word details

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,55 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:japanese_vocabulary/ui/screens/word_item.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
+
+import '../../data/models/review.dart';
+import '../../data/models/word.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  Word word() {
+    final Word word = Word(
+      id: 0,
+      text: "言葉",
+      reading: "ことば",
+      jlpt: 5,
+      meaning: "word; phrase; expression; term",
+      pos: "Noun",
+    );
+
+    final mRev = Review(
+      id: 0,
+      ef: 2.5,
+      correctAnswers: 0,
+      incorrectAnswers: 0,
+      interval: 0,
+      nextDate: null,
+      repetition: 0,
+      type: "meaning",
+    );
+
+    final rRev = Review(
+      id: 0,
+      ef: 2.5,
+      correctAnswers: 0,
+      incorrectAnswers: 0,
+      interval: 0,
+      nextDate: null,
+      repetition: 0,
+      type: "reading",
+    );
+
+    mRev.word.target = word;
+    rRev.word.target = word;
+
+    word.meaningReview.target = mRev;
+    word.readingReview.target = rRev;
+
+    return word;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final List<Word> words = [word(), word(), word()];
     return Scaffold(
       appBar: AppBar(
-        title: Text("View words", style: TextStyle(fontSize: 25)),
+        title: const Text("View words"),
       ),
       body: ListView.builder(
+        itemCount: words.length,
         itemBuilder: (BuildContext, index) {
-          return Card(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(top: 8, left: 5, bottom: 8),
-                      child: Text("Word list",
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold)),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 12, bottom: 8),
-                      child: Text("Next date review"),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(top: 4, right: 4, bottom: 4),
-                      child: CircularPercentIndicator(
-                        radius: 32.0,
-                        lineWidth: 32 / 4,
-                        percent: .90,
-                        center: new Text("90%"),
-                        progressColor: Colors.green,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          );
+          final Word? word = words[index];
+          return WordItem(word: word);
         },
         padding: const EdgeInsets.all(8),
         shrinkWrap: true,
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FloatingActionButton(
+          child: const Icon(Icons.add),
+          backgroundColor: Colors.amber,
+          onPressed: () {},
+        ),
       ),
     );
   }
