14c25098fff7d63b03a356479e65f2ccf94f75ea
Mirco Malanchini <mircomalanchini@hotmail.it>
2022-04-05 16:07:07 +0200

    style: lint correction

--- test/word_list_test.dart
+++ test/word_list_test.dart
@@ -3,14 +3,6 @@ import 'package:japanese_vocabulary/data/models/word.dart';
 import 'package:japanese_vocabulary/ui/screens/word_item.dart';
 
 void main() {
-  final Word word = Word(
-      id: 0,
-      text: "普通",
-      reading: "ふつう",
-      jlpt: 4,
-      meaning: "normal; ordinary; regular",
-      pos: "Noun, Na-adjective");
-
   testWidgets("Test widget view list, null case", (WidgetTester tester) async {
     await tester.pumpWidget(const WordItem(word: null));
 
