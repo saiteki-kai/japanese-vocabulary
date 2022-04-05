6531f15abeff06498ec9c53b176a60cf6b01c4b1
Mirco Malanchini <mircomalanchini@hotmail.it>
2022-04-05 17:29:56 +0200

    feat: add circular percent indicator test case

--- test/word_list_test.dart
+++ test/word_list_test.dart
@@ -1,11 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:japanese_vocabulary/ui/screens/word_item.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 void main() {
   testWidgets("Test widget view list, null case", (WidgetTester tester) async {
     await tester.pumpWidget(const WordItem(word: null));
 
-    final textFinder = find.text("Error");
-    expect(textFinder, findsOneWidget);
+    expect(find.byType(CircularPercentIndicator), findsWidgets);
   });
 }
