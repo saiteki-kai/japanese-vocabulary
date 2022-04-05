f84a0fc597b47c8106160df1295e061947edecfb
Mirco Malanchini <mircomalanchini@hotmail.it>
2022-04-05 15:58:06 +0200

    style: add response in error case

--- lib/ui/screens/word_item.dart
+++ lib/ui/screens/word_item.dart
@@ -22,7 +22,7 @@ class WordItem extends StatelessWidget {
             children: [
               Padding(
                 padding: const EdgeInsets.only(top: 8, left: 5, bottom: 8),
-                child: Text(word?.text ?? "",
+                child: Text(word?.text ?? "Error",
                     style: const TextStyle(
                         fontSize: 20, fontWeight: FontWeight.bold)),
               ),
