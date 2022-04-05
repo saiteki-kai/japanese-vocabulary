98f713f14899017419ea9c0068599dca5ce82fa4
Giuseppe <giuseppe.magazu@gmail.com>
2022-04-05 20:32:20 +0200

    test: add widget tests for review session

---
+++ test/review_item_test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:japanese_vocabulary/data/models/review.dart';
+import 'package:japanese_vocabulary/ui/screens/review_screen/widgets/review_item.dart';
+import 'package:japanese_vocabulary/ui/screens/review_screen/widgets/review_type_tag.dart';
+
+import 'utils/review.dart';
+
+void main() {
+  final readingReview = ReviewUtils.readingReviewWithWord;
+  final meaningReview = ReviewUtils.meaningReviewWithWord;
+
+  for (Review review in [readingReview, meaningReview]) {
+    testWidgets("check reading review information",
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: ReviewItem(
+            hidden: ValueNotifier(false),
+            review: review,
+            onToggleAnswer: () {},
+          ),
+        ),
+      );
+
+      // check word text
+      final wordTextFinder = find.text(review.word.target!.text);
+      expect(
+        wordTextFinder,
+        findsOneWidget,
+        reason: "the word text should be ${review.word.target!.text}",
+      );
+
+      // check if the type displayed is correct
+      final typeTextFinder = find.widgetWithText(
+        ReviewTypeTag,
+        review.type.toUpperCase(),
+      );
+      expect(
+        typeTextFinder,
+        findsOneWidget,
+        reason: "the review type should be ${review.type}",
+      );
+    });
+  }
+}
---
+++ test/review_session_test.dart
@@ -0,0 +1,236 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:group_button/group_button.dart';
+import 'package:japanese_vocabulary/ui/screens/review_screen/widgets/show_button.dart';
+import 'package:japanese_vocabulary/bloc/review_bloc.dart';
+import 'package:japanese_vocabulary/data/models/review.dart';
+import 'package:japanese_vocabulary/ui/screens/review_screen/widgets/next_review_button.dart';
+import 'package:japanese_vocabulary/ui/screens/review_screen/widgets/review_answer.dart';
+import 'package:japanese_vocabulary/ui/screens/review_screen/widgets/review_item.dart';
+import 'package:japanese_vocabulary/ui/screens/review_screen/widgets/review_session.dart';
+import 'package:mocktail/mocktail.dart';
+
+import 'utils/review.dart';
+
+class MockReviewBloc extends MockBloc<ReviewEvent, ReviewState>
+    implements ReviewBloc {}
+
+void main() {
+  late ReviewBloc bloc;
+
+  setUp(() {
+    bloc = MockReviewBloc();
+  });
+
+  tearDown(() {
+    bloc.close();
+  });
+
+  setUpWidget(WidgetTester tester, Review review, {bool isLast = false}) async {
+    when(() => bloc.state).thenReturn(ReviewInitial());
+
+    // Provide a review for the test
+    final state = ReviewLoaded(review: review, isLast: isLast);
+    when(() => bloc.state).thenReturn(state);
+
+    await tester.pumpAndSettle();
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: BlocProvider.value(
+          value: bloc,
+          child: const ReviewSession(),
+        ),
+      ),
+    );
+  }
+
+  testWidgets("check correct answer", (WidgetTester tester) async {
+    await setUpWidget(tester, ReviewUtils.meaningReviewWithWord);
+
+    final answer = ReviewUtils.meaningReviewWithWord.word.target?.meaning;
+    expect(answer, isNotNull);
+
+    final reviewAnswerFinder = find.widgetWithText(ReviewAnswer, answer!);
+
+    expect(
+      reviewAnswerFinder,
+      findsOneWidget,
+      reason: "The correct answer should be $answer.",
+    );
+  });
+
+  testWidgets("show / hide answer", (WidgetTester tester) async {
+    await setUpWidget(tester, ReviewUtils.review1);
+
+    final reviewItemFinder = find.byType(ReviewItem);
+    expect(
+      reviewItemFinder,
+      findsOneWidget,
+      reason: "There should be one ReviewItem.",
+    );
+
+    final showButtonFinder = find.widgetWithText(ShowButton, "SHOW");
+    expect(
+      showButtonFinder,
+      findsOneWidget,
+      reason: "The show button should exist and display 'show' as text.",
+    );
+
+    final hideButtonFinder = find.widgetWithText(ShowButton, "HIDE");
+    expect(
+      hideButtonFinder,
+      findsNothing,
+      reason: "The show button should exist and display 'hide' as text.",
+    );
+
+    final answerFinder = find.byType(ReviewAnswer);
+    expect(
+      answerFinder,
+      findsOneWidget,
+      reason: "There should be one ReviewAnswer.",
+    );
+
+    final reviewAnswer = tester.widget<ReviewAnswer>(answerFinder.first);
+    expect(
+      reviewAnswer.hidden.value,
+      true,
+      reason: "The value should be true.",
+    );
+
+    // Show answer
+    await tester.tap(showButtonFinder.first);
+    await tester.pump();
+
+    expect(
+      reviewAnswer.hidden.value,
+      false,
+      reason: "The value should be false.",
+    );
+
+    expect(hideButtonFinder, findsOneWidget);
+    expect(showButtonFinder, findsNothing);
+
+    // Hide answer
+    await tester.tap(hideButtonFinder.first);
+    await tester.pump();
+
+    expect(
+      reviewAnswer.hidden.value,
+      true,
+      reason: "The value should be true.",
+    );
+
+    expect(hideButtonFinder, findsNothing);
+    expect(showButtonFinder, findsOneWidget);
+  });
+
+  group("check next / summary button", () {
+    Future<List<Finder>> findButtons(find) async {
+      final nextReviewButtonFinder = find.byType(NextReviewButton);
+
+      final nextButtonFinder = find.descendant(
+        of: nextReviewButtonFinder.first,
+        matching: find.text("NEXT"),
+      );
+      final summaryButtonFinder = find.descendant(
+        of: nextReviewButtonFinder.first,
+        matching: find.text("SUMMARY"),
+      );
+
+      return [nextButtonFinder, summaryButtonFinder];
+    }
+
+    testWidgets("show next button when the current review is not the last",
+        (WidgetTester tester) async {
+      await setUpWidget(tester, ReviewUtils.review1, isLast: false);
+
+      final buttons = await findButtons(find);
+      final nextButtonFinder = buttons[0];
+      final summaryButtonFinder = buttons[1];
+
+      expect(nextButtonFinder, findsOneWidget);
+      expect(summaryButtonFinder, findsNothing);
+    });
+
+    testWidgets("show summary button when the current review is the last",
+        (WidgetTester tester) async {
+      await setUpWidget(tester, ReviewUtils.review1, isLast: true);
+
+      final buttons = await findButtons(find);
+      final nextButtonFinder = buttons[0];
+      final summaryButtonFinder = buttons[1];
+
+      expect(nextButtonFinder, findsNothing);
+      expect(summaryButtonFinder, findsOneWidget);
+    });
+  });
+
+  testWidgets("the review is updated when the next button is tapped.",
+      (WidgetTester tester) async {
+    final review = ReviewUtils.meaningReviewWithWord;
+    await setUpWidget(tester, review);
+
+    final wordText = review.word.target!.text;
+
+    final wordTextFinder = find.widgetWithText(ReviewItem, wordText);
+    expect(
+      wordTextFinder,
+      findsOneWidget,
+      reason: "There word text of the current review should be $wordText",
+    );
+
+    final answerFinder = find.byType(ReviewAnswer);
+    expect(
+      answerFinder,
+      findsOneWidget,
+      reason: "There should be one ReviewAnswer.",
+    );
+
+    final showButtonFinder = find.widgetWithText(TextButton, "SHOW");
+    expect(
+      showButtonFinder,
+      findsOneWidget,
+      reason: "The show button should exist.",
+    );
+
+    // Show answer
+    await tester.tap(showButtonFinder.first);
+    await tester.pump();
+
+    final groupButtonFinder = find.byType(GroupButton);
+    expect(
+      groupButtonFinder,
+      findsOneWidget,
+      reason: "The GroupButton widget should exist.",
+    );
+    final button4 = find.descendant(
+      of: groupButtonFinder.first,
+      matching: find.text("4"),
+    );
+
+    // Select the quality value 4
+    await tester.tap(button4.first);
+    await tester.pump();
+
+    final nextReviewTextFinder = find.widgetWithText(TextButton, "NEXT");
+    expect(
+      nextReviewTextFinder,
+      findsOneWidget,
+      reason: "The next button should exist.",
+    );
+
+    final nextReviewButtonFinder = find.descendant(
+      of: find.byType(NextReviewButton),
+      matching: find.byType(TextButton),
+    );
+
+    // Tap on next button
+    await tester.tap(nextReviewButtonFinder.first);
+    await tester.pump();
+
+    verify(() => bloc.add(ReviewSessionUpdated(review, 4))).called(1);
+  });
+}
