89506ecacbe63e4efdc18c04d619b84113041a1b
Mirco Malanchini <mircomalanchini@hotmail.it>
2022-04-06 10:09:52 +0200

    test: add test for get words

--- test/word_list_test.dart
+++ test/word_list_test.dart
@@ -1,11 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:japanese_vocabulary/ui/screens/word_item.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 
 void main() {
   testWidgets("Test widget view list, null case", (WidgetTester tester) async {
-    await tester.pumpWidget(const WordItem(word: null));
+    await tester.pumpWidget(
+        const MaterialApp(home: Scaffold(body: WordItem(word: null))));
 
-    expect(find.byType(CircularPercentIndicator), findsWidgets);
+    expect(find.byType(CircularPercentIndicator), findsOneWidget);
+    expect(find.text("Error"), findsOneWidget);
   });
 }
