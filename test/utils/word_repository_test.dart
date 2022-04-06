ea17c24565ff6c78ae2f75df08e39cec128b702d
Mario Marino <marinomario.17@gmail.com>
2022-04-06 10:56:36 +0200

    refactor:changed word repository var name in test file

--- test/utils/word_repository_test.dart
+++ test/utils/word_repository_test.dart
@@ -9,40 +9,40 @@ import 'word_instances.dart';
 void main() async {
   final store = await AppDatabase.instance.store;
   final box = store.box<Word>();
-  final word_repository = WordRepository();
-  final review_box = store.box<Review>();
+  final wordRepository = WordRepository();
+  final reviewBox = store.box<Review>();
 
   setUp(() {
     //print("setup");
     box.removeAll();
-    review_box.removeAll();
+    reviewBox.removeAll();
     //print(review_box.getAll().toString());
   });
   tearDown(() {
     //print("teardown");
     box.removeAll();
-    review_box.removeAll();
+    reviewBox.removeAll();
     //print(review_box.getAll().toString());
   });
 
   test('Testing simple insertion', () async {
     expect(box.getAll().length, 0);
-    final res = await word_repository.addWord(WordTestInstances.word1);
+    final res = await wordRepository.addWord(WordTestInstances.word1);
     expect(res, 1, reason: "Id should've been 1");
     expect(box.getAll().length, 1, reason: "# elements should've been 1");
   });
 
   test('Testing word-reviews relations', () async {
-    final res = await word_repository.addWord(WordTestInstances.word1);
+    final res = await wordRepository.addWord(WordTestInstances.word1);
 
     final word = box.get(res);
     expect(word?.meaningReview.target, isNotNull);
     expect(word?.readingReview.target, isNotNull);
     expect(box.getAll().length, 1);
-    expect(review_box.getAll().length, 2);
+    expect(reviewBox.getAll().length, 2);
     //print(review_box.getAll().toString());
     expect(
-        review_box
+        reviewBox
             .getAll()
             .where((element) => element.word.targetId == res)
             .length,
