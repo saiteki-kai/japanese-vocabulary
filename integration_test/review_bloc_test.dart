f9e23ec64ff4caced7ef36bf4db54c8b2ec8c403
Giuseppe <giuseppe.magazu@gmail.com>
2022-05-13 18:22:35 +0200

    test: fix ids and delete the database after each test

--- integration_test/review_bloc_test.dart
+++ integration_test/review_bloc_test.dart
@@ -4,27 +4,32 @@ import 'package:japanese_vocabulary/bloc/review_bloc.dart';
 import 'package:japanese_vocabulary/data/app_database.dart';
 import 'package:japanese_vocabulary/data/models/review.dart';
 import 'package:japanese_vocabulary/data/repositories/review_repository.dart';
+import 'package:objectbox/objectbox.dart';
 
 import '../test/utils/params.dart';
 
 void main() async {
+  late Store store;
+  late Box<Review> box;
   late ReviewBloc bloc;
 
-  final store = await AppDatabase.instance.store;
-  final box = store.box<Review>();
+  setUp(() async {
+    store = await AppDatabase.instance.store;
+    box = store.box<Review>();
 
-  setUp(() {
     bloc = ReviewBloc(repository: ReviewRepository(box: Future.value(box)));
+
     box.removeAll();
     box.put(nullDateReview);
     box.put(review1);
     box.put(review2);
   });
 
-  tearDown(() {
+  tearDown(() async {
     bloc.close();
     box.removeAll();
-    AppDatabase.instance.deleteDatabase();
+    store.close();
+    await AppDatabase.instance.deleteDatabase();
   });
 
   blocTest<ReviewBloc, ReviewState>(
@@ -33,7 +38,7 @@ void main() async {
     act: (bloc) => bloc.add(ReviewSessionStarted()),
     expect: () => <ReviewState>[
       ReviewLoading(),
-      ReviewLoaded(review: nullDateReview, isLast: false),
+      ReviewLoaded(review: nullDateReview..id = 1, isLast: false),
     ],
   );
 
@@ -45,8 +50,8 @@ void main() async {
       ..add(ReviewSessionUpdated(review: review1, quality: 4)),
     expect: () => <ReviewState>[
       ReviewLoading(),
-      ReviewLoaded(review: nullDateReview, isLast: false),
-      ReviewLoaded(review: review1, isLast: true),
+      ReviewLoaded(review: nullDateReview..id = 1, isLast: false),
+      ReviewLoaded(review: review1..id = 2, isLast: true),
     ],
   );
 
@@ -59,8 +64,8 @@ void main() async {
       ..add(ReviewSessionUpdated(review: review2, quality: 4)),
     expect: () => <ReviewState>[
       ReviewLoading(),
-      ReviewLoaded(review: nullDateReview, isLast: false),
-      ReviewLoaded(review: review1, isLast: true),
+      ReviewLoaded(review: nullDateReview..id = 1, isLast: false),
+      ReviewLoaded(review: review1..id = 2, isLast: true),
       ReviewFinished(),
     ],
   );
@@ -68,7 +73,7 @@ void main() async {
   blocTest<ReviewBloc, ReviewState>(
     'emits [ReviewLoading, ReviewError] when ReviewSessionStarted is added and session is empty.',
     build: () => bloc,
-    setUp: box.removeAll,
+    setUp: () => box.removeAll(),
     act: (bloc) => bloc..add(ReviewSessionStarted()),
     expect: () => <ReviewState>[
       ReviewLoading(),
