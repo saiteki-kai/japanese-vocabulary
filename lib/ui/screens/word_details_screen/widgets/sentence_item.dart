65cf547d879fdf6c6cc8387608d33cde71f542e4
Mirco Malanchini <mircomalanchini@hotmail.it>
2022-05-16 14:26:46 +0200

    [skip ci] feat: add view word sentences

---
+++ lib/ui/screens/word_details_screen/widgets/sentence_item.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import '../../../../data/models/sentence.dart';
+
+class SentenceItem extends StatelessWidget {
+  const SentenceItem({
+    Key? key,
+    required this.sentence,
+  }) : super(key: key);
+
+  final Sentence sentence;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: InkWell(
+        onTap: () => _onWordPressed(context),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    sentence.text,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    sentence.translation,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onWordPressed(context) {
+    //AutoRouter.of(context).push(WordDetailsScreen(wordId: word.id));
+  }
+}
