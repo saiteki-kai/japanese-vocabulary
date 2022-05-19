d63064ba49b5f5d002c7c0e3b9a605f336318347
Giuseppe <giuseppe.magazu@gmail.com>
2022-05-19 19:23:44 +0200

    [skip ci] refactor: move the getNextReadingHint function in the Hint class and add empty and fromReading constructors

--- lib/utils/hints.dart
+++ lib/utils/hints.dart
@@ -1,41 +1,65 @@
 class Hint {
   /// Creates a [Hint] with a [text] to show and the available quality [values].
-  const Hint({required this.text, required this.values});
+  const Hint({
+    required this.text,
+    required this.n,
+    required this.max,
+    required this.values,
+  })  : assert(n >= 0),
+        assert(n <= max);
 
   /// Hint message to show.
   final String text;
 
+  final int n;
+
+  final int max;
+
   /// Available quality values after this hint is given.
   final List<int> values;
-}
 
-/// Returns a [Hint] of a given word [reading] showing [n] characters.
-///
-/// Returns null if [reading] has 1 or less characters.
-///
-/// Throws a [FormatException] when the number of characters [n] is greater than
-/// the length of [reading] or less than or equal to 0.
-Hint? getReadingHint(String reading, int n) {
-  final length = reading.length;
-  final ratio = n / length;
+  Hint.empty()
+      : text = "",
+        n = 0,
+        max = 0,
+        values = [],
+        super();
 
-  if (length <= 1) {
-    return null;
-  } else if (n <= 0) {
-    throw const FormatException("'hints' must be greater than 0.");
-  } else if (n > length) {
-    throw const FormatException("No more hints available.");
+  Hint.fromReading(String reading)
+      : text = "",
+        n = 0,
+        max = reading.length,
+        values = const [0, 1, 2, 3, 4, 5],
+        super();
+
+  /// Returns a [Hint] of a given word [reading] showing progressively more
+  /// characters.
+  Hint getNextReadingHint(String reading) {
+    final n = this.n + 1;
+
+    final length = reading.length;
+    final ratio = n / length;
+
+    // TODO: handle exceptions
+    if (n > length || length == 1) {
+      return Hint.empty();
+    }
+
+    List<int> values = [];
+
+    if (ratio <= 0.4) {
+      values = [0, 1, 2, 3, 4];
+    } else if (ratio <= 0.5) {
+      values = [0, 1, 2, 3];
+    } else if (ratio <= 0.8) {
+      values = [0, 1, 2];
+    }
+
+    return Hint(
+      text: reading.substring(0, n),
+      n: n,
+      max: reading.length,
+      values: values,
+    );
   }
-
-  List<int> values = [];
-
-  if (ratio <= 0.4) {
-    values = [0, 1, 2, 3, 4];
-  } else if (ratio <= 0.5) {
-    values = [0, 1, 2, 3];
-  } else if (ratio <= 0.8) {
-    values = [0, 1, 2];
-  }
-
-  return Hint(text: reading.substring(0, n), values: values);
 }
