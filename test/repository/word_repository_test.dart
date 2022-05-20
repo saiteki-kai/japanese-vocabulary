dd3415a46fd140fbda35f6b5746593cd40b68692
davide.pietrasanta <davide.pietrasanta@virgilio.it>
2022-05-20 12:29:11 +0200

    test: Fixed 'simple edit' unit test.

--- test/repository/word_repository_test.dart
+++ test/repository/word_repository_test.dart
@@ -72,35 +72,20 @@ void main() async {
     test('simple edit', () async {
       when(() => box.put(any()))
           .thenAnswer((inv) => inv.positionalArguments[0].id);
-      when(() => box.put(word5)).thenReturn(5);
-      when(() => box.get(5)).thenReturn(word5);
 
-      final id = await repo.addWord(word5);
-      expect(id, 5, reason: "Id should've been 5");
-      verify(() => box.put(any())).called(1);
+      final id = await repo.addWord(word1);
+      expect(id, word1.id, reason: "Id should've been ${word1.id}");
 
-      final word = await repo.getWord(5);
+      final word = word1;
 
-      word!.text = "Gracias";
+      word.text = "Gracias";
       word.reading = "Gra see uhs";
       word.jlpt = 1;
       word.meaning = "Thanks";
       word.pos = "n, vi";
 
-      when(() => box.put(word)).thenReturn(5);
-      when(() => box.get(5)).thenReturn(word);
-
-      final id2 = await repo.addWord(word);
-      final wordEdited = await repo.getWord(id2);
-
-      expect(wordEdited?.text, "Gracias");
-      expect(wordEdited?.reading, "Gra see uhs");
-      expect(wordEdited?.jlpt, 1);
-      expect(wordEdited?.meaning, "Thanks");
-      expect(wordEdited?.pos, "n, vi");
-
-      expect(id2, 5, reason: "Id should've been 5");
-      verify(() => box.put(any())).called(1);
+      final id2 = await repo.addWord(word1);
+      expect(id2, id, reason: "Id should've been $id2");
     });
   });
 }
