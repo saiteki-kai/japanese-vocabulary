259e299a619f88a69f20aca806208ee710f31522
MagazzuGaetano <gaetanomagazzu3@gmail.com>
2022-05-24 18:40:33 +0200

    [skip ci] test: add meaning hint widget tests

--- test/utils/params.dart
+++ test/utils/params.dart
@@ -1,4 +1,5 @@
 import 'package:japanese_vocabulary/data/models/review.dart';
+import 'package:japanese_vocabulary/data/models/sentence.dart';
 import 'package:japanese_vocabulary/data/models/word.dart';
 
 Word get emptyWord {
@@ -98,6 +99,17 @@ Review get nowReview => createReviewByDate(DateTime.now());
 Review get review1 => createReviewByDate(DateTime(DateTime.now().year - 1));
 Review get review2 => createReviewByDate(DateTime(DateTime.now().year + 1));
 
+final sentence1 = Sentence(
+    text: '言葉と行動は一致すべきものだが、実行は難しい。',
+    translation:
+        'Your words are supposed to correspond to your actions, but that is not easy to put into practice.');
+final sentence2 = Sentence(
+    text: '彼女は図書館の利用許可を与えられた。',
+    translation: 'She was accorded permission to use the library.');
+final sentence3 = Sentence(
+    text: '麺はふつう小麦粉から作られる。',
+    translation: 'Noodles are usually made from wheat.');
+
 Review get readingReviewWithWord {
   return createReviewByDate(
     null,
@@ -107,9 +119,33 @@ Review get readingReviewWithWord {
 }
 
 Review get meaningReviewWithWord {
+  final Word word = word1;
+  word.sentences.addAll([sentence1, sentence2, sentence3]);
   return createReviewByDate(
     null,
     type: "meaning",
-    word: word1,
+    word: word,
+  );
+}
+
+Word get wordWith3Sentences {
+  final word =  Word(
+    text: "言葉",
+    reading: "ことば",
+    jlpt: 5,
+    meaning: "word; phrase; expression; term",
+    pos: "Noun",
+  );
+  word.sentences.addAll([sentence1, sentence2, sentence3]);
+  return word;
+}
+
+Word get wordWithoutSentences {
+  return Word(
+    text: "復習",
+    reading: "ふくしゅう",
+    jlpt: 4,
+    meaning: "review (of learned material); revision",
+    pos: "Noun, Suru verb",
   );
 }
--- test/widgets/review_hint_button_test.dart
+++ test/widgets/review_hint_button_test.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:japanese_vocabulary/data/models/sentence.dart';
 import 'package:japanese_vocabulary/ui/screens/review_session_screen/widgets/review_hint_button.dart';
-import 'package:japanese_vocabulary/utils/hints.dart';
+import 'package:japanese_vocabulary/utils/hint.dart';
+
+import '../utils/params.dart';
 
 void main() {
   setUpWidget(WidgetTester tester, Hint hint) async {
@@ -23,35 +26,79 @@ void main() {
     );
   }
 
-  testWidgets("hint initialized", (WidgetTester tester) async {
-    const hint = Hint(n: 0, max: 3, text: "", values: []);
+  group('reading hint', () {
+    testWidgets("initialized", (WidgetTester tester) async {
+      final hint = ReadingHint(n: 0, max: 3, text: "", values: []);
 
-    await setUpWidget(tester, hint);
+      await setUpWidget(tester, hint);
 
-    expect(find.text("3"), findsOneWidget);
+      expect(find.text("3"), findsOneWidget);
+    });
+
+    testWidgets("first hint", (WidgetTester tester) async {
+      final hint = ReadingHint(n: 1, max: 3, text: "に", values: []);
+
+      await setUpWidget(tester, hint);
+
+      expect(find.text("2"), findsOneWidget);
+    });
+
+    testWidgets("second hint", (WidgetTester tester) async {
+      final hint = ReadingHint(n: 2, max: 3, text: "にほ", values: []);
+
+      await setUpWidget(tester, hint);
+
+      expect(find.text("1"), findsOneWidget);
+    });
+
+    testWidgets("last hint", (WidgetTester tester) async {
+      final hint = ReadingHint(n: 3, max: 3, text: "にほん", values: []);
+
+      await setUpWidget(tester, hint);
+
+      expect(find.text("0"), findsOneWidget);
+    });
   });
 
-  testWidgets("first hint", (WidgetTester tester) async {
-    const hint = Hint(n: 1, max: 3, text: "に", values: []);
+  group('meaning hint', () {
+    testWidgets("initialized", (WidgetTester tester) async {
+      final List<Sentence> sentences = [];
+      final hint =
+          MeaningHint(n: 0, max: 3, values: [0,1,2,3,4,5], currSentences: sentences);
 
-    await setUpWidget(tester, hint);
+      await setUpWidget(tester, hint);
 
-    expect(find.text("2"), findsOneWidget);
-  });
+      expect(find.text("3"), findsOneWidget);
+    });
 
-  testWidgets("second hint", (WidgetTester tester) async {
-    const hint = Hint(n: 2, max: 3, text: "にほ", values: []);
+    testWidgets("first hint", (WidgetTester tester) async {
+      final List<Sentence> sentences = [sentence1];
+      final hint =
+          MeaningHint(n: 1, max: 3, values: [0,1,2,3,4], currSentences: sentences);
 
-    await setUpWidget(tester, hint);
+      await setUpWidget(tester, hint);
 
-    expect(find.text("1"), findsOneWidget);
-  });
+      expect(find.text("2"), findsOneWidget);
+    });
 
-  testWidgets("last hint", (WidgetTester tester) async {
-    const hint = Hint(n: 3, max: 3, text: "にほん", values: []);
+    testWidgets("second hint", (WidgetTester tester) async {
+      final List<Sentence> sentences = [sentence1, sentence2];
+      final hint =
+          MeaningHint(n: 2, max: 3, values: [0,1,2,3], currSentences: sentences);
 
-    await setUpWidget(tester, hint);
+      await setUpWidget(tester, hint);
 
-    expect(find.text("0"), findsOneWidget);
+      expect(find.text("1"), findsOneWidget);
+    });
+
+    testWidgets("last hint", (WidgetTester tester) async {
+      final List<Sentence> sentences = [sentence1, sentence2, sentence3];
+      final hint =
+          MeaningHint(n: 3, max: 3, values: [0,1,2,3], currSentences: sentences);
+
+      await setUpWidget(tester, hint);
+
+      expect(find.text("0"), findsOneWidget);
+    });
   });
 }
