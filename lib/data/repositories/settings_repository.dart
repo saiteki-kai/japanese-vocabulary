6fcaf51c826851391f654a5a520beb194f375d2c
Giuseppe <giuseppe.magazu@gmail.com>
2022-05-25 11:02:06 +0200

    feat: add settings repository

---
+++ lib/data/repositories/settings_repository.dart
@@ -0,0 +1,21 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../models/settings.dart';
+
+class SettingsRepository {
+  const SettingsRepository({required this.prefs});
+
+  final Future<SharedPreferences> prefs;
+
+  Future<Settings?> getSettings() async {
+    final json = (await prefs).getString("settings");
+    if (json != null) {
+      return Settings.fromJson(json);
+    }
+    return null;
+  }
+
+  Future setSettings(Settings settings) async {
+    (await prefs).setString("settings", settings.toJson());
+  }
+}
