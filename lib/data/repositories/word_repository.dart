2df000dda1b9ba42fa0eaaf846fe9c0e4cacd529
Giuseppe <giuseppe.magazu@gmail.com>
2022-06-04 16:39:27 +0200

    style: remove default params

--- lib/data/repositories/word_repository.dart
+++ lib/data/repositories/word_repository.dart
@@ -14,30 +14,17 @@ class WordRepository {
   /// It creates a meaning and a reading review associated to this word
   Future<int> addWord(Word word) async {
     final box = await _box;
+
     final readingReview = word.readingReview;
     final meaningReview = word.meaningReview;
-    if (readingReview.target == null) {
-      final Review meaning = Review(
-        ef: 2.5,
-        interval: 0,
-        repetition: 0,
-        correctAnswers: 0,
-        incorrectAnswers: 0,
-        type: 'meaning',
-        nextDate: DateTime.now(),
-      );
-      final Review reading = Review(
-        ef: 2.5,
-        interval: 0,
-        repetition: 0,
-        correctAnswers: 0,
-        incorrectAnswers: 0,
-        type: 'reading',
-        nextDate: DateTime.now(),
-      );
+
+    if (readingReview.target == null && meaningReview.target == null) {
+      final Review meaning = Review(type: 'meaning', nextDate: DateTime.now());
+      final Review reading = Review(type: 'reading', nextDate: DateTime.now());
 
       meaningReview.target = meaning;
       readingReview.target = reading;
+
       meaning.word.target = word;
       reading.word.target = word;
     }
